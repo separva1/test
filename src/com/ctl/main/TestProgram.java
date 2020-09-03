8adaccfcdfbd6e915a603f2e1b8cfe237c7da422
Sejala Parvathappa <separva1@in.ibm.com>
2020-09-03 16:45:32 +0530

    intial commit

---
+++ src/com/ctl/main/TestProgram.java
@@ -0,0 +1,10 @@
+package com.ctl.main;
+
+public class TestProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("java first program");
+	}
+
+}
